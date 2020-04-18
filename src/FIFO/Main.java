118aa1b878dafa489c1276c1272857674d37c601
LukeHuckman <lukeh@outlook.my>
2020-04-18 18:31:18 +0800

    - Added the ability to set frame and page size. - Use array instead of LinkedList. - Bugfixes.

--- src/FIFO/Main.java
+++ src/FIFO/Main.java
@@ -4,38 +4,41 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
-    private static LinkedList<String> memory = new LinkedList();
+    private static String[] memory;
     private static String value = "";
-    private static int index = 0;
+    private static Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        main: while(true){
-            System.out.print("FIFO simulation\n"
-                    + "Enter a string: ");
+        int framenum, pagenum;
+        System.out.print("FIFO simulation\nNumber of frames: ");
+        memory = new String[Integer.parseInt(s.nextLine())];
+        System.out.print("Number of pages: ");
+        pagenum = Integer.parseInt(s.nextLine());
+        page(memory, pagenum);
+    }
+    
+    public static void page(String[] memory, int pagenum){
+        int index = 0;
+  main: for(int i=0;i<pagenum;i++){
+            System.out.print("\nEnter a String: ");
             value = s.nextLine();
-            if(index>2) //Frame size is 3
+            if(index>=memory.length)
                 index=0;
-            try{
-                for(int i=0;i<memory.size();i++)
-                    //Checks if the value already exists in memory
-                    if(value.equals(memory.get(i))){
-                        System.out.println("\n"+frame());
-                        continue main; //Skips if the value exists
-                    }
-                memory.set(index, value); //Replaces the value in the frame
-                index++;
-            }catch(IndexOutOfBoundsException e){
-                //Error handling: Adds new value if no value exists in the frame
-                memory.add(index, value);
-                index++;
-            }
-            System.out.println("\n"+frame());
+            for(int j=0;j<memory.length-2;j++)
+                //Checks if the value already exists in memory
+                if(value.equals(memory[j])){
+                    System.out.println("\n"+frameDisp());
+                    continue main; //Skips if the value exists
+                }
+            memory[index]=value; //Replaces the value in the frame
+            index++;
+            System.out.println("\n"+frameDisp());
         }
     }
-    public static String frame(){ //Displays the page frames
+    
+    public static String frameDisp(){ //Displays the page frames
         String out = "";
-        for(int i=0;i<memory.size();i++)
-            out+="\t"+memory.get(i)+"\n";
+        for(int i=0;i<memory.length;i++)
+            out+="\t"+memory[i]+"\n";
         return "Page frames:\n"+out+"\n";
     }
 }
