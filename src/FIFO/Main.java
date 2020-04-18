e035a06f4a54107f59718b724bd213818968b884
LukeHuckman <lukeh@outlook.my>
2020-04-18 08:01:14 +0800

    - Redesigned code to match Page Replacement scheme. - Bugfixes.

--- src/FIFO/Main.java
+++ src/FIFO/Main.java
@@ -6,43 +6,34 @@ import java.util.Scanner;
 public class Main {
     private static LinkedList<String> memory = new LinkedList();
     private static String value = "";
+    private static int index = 0;
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        while(true){
-            System.out.print("FIFO simulation:\n"
-                    + "1 - Add new data"
-                    + "\n2 - Remove data"
-                    + "\n3 - Check memory"
-                    + "\n4 - Exit"
-                    + "\n\n>");
+        main: while(true){
+            System.out.print("FIFO simulation\n"
+                    + "Enter a string: ");
             value = s.nextLine();
-            switch (value) {
-                case "1":
-                    System.out.print("Enter value: ");
-                    value = s.nextLine();
-                    memory.addLast(value);
-                    System.out.println("\n"+stack());
-                    break;
-                case "2":
-                    if(memory.isEmpty())
-                        System.out.println("\nNo data to retrieve.\n");
-                    else
-                        System.out.println("\nRetrieved: \""+memory.removeFirst()+"\"\n\n"+stack());
-                    break;
-                case "3":
-                    System.out.println(stack());
-                    break;
-                case "4":
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid option, try again.");
+            if(index>2)
+                index=0;
+            try{
+                for(int i=0;i<memory.size();i++)
+                    if(value.equals(memory.get(i))){
+                        System.out.println("\n"+frame());
+                        continue main;
+                    }
+                memory.set(index, value);
+                index++;
+            }catch(IndexOutOfBoundsException e){
+                memory.add(index, value);
+                index++;
             }
+            System.out.println("\n"+frame());
         }
     }
-    public static String stack(){
+    public static String frame(){
         String out = "";
-        for(int i=memory.size()-1;i>=0;i--)
+        for(int i=0;i<memory.size();i++)
             out+="\t"+memory.get(i)+"\n";
-        return "Memory:\n"+out+"\n";
+        return "Page frames:\n"+out+"\n";
     }
 }
